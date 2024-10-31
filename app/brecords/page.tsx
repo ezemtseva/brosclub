6b27e6585bf86dae444c7bb588e85b579fa0eb66
Yury Loguinov <yuryloguinov@MacBook-Pro-Yury.local>
2024-10-31 22:04:04 +0100

    Game day upates

--- app/brecords/page.tsx
+++ app/brecords/page.tsx
@@ -62,7 +62,7 @@ const sections = [
   {
     title: "Geo Guessr",
     records: [
-      <><UnderlinedPlayer name="Vanilla" /> holds the record for the most accurate guess when the location was known – 17 m, Hagia Sophia (Istanbul), 2024/25</>,
+      <><UnderlinedPlayer name="Vanilla" /> holds the record for the most accurate guess when the location was known – 4 m, Plaza Mayor (Arequipa), 2024/25</>,
       <><UnderlinedPlayer name="Vanilla" /> holds the record for the most accurate guess when the location was unknown – 910 m, Monastery of Santa Catalina (Arequipa), 2024/25</>,
       <><UnderlinedPlayer name="Vanilla" /> hits first 5k in the competition - 129 m, Belgrade 2024/25</>
     ]

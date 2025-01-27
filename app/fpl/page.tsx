f3a1837cee05fe7bf61970371e249edda091c5a2
Yury <yuryloguinov@192.168.1.13>
2025-01-27 09:32:15 +0100

    Weekly updates

--- app/fpl/page.tsx
+++ app/fpl/page.tsx
@@ -103,6 +103,7 @@ export default async function FPLPage() {
     )
 
     const images = [
+      { src: "/imgs/fpl/fpl23.png", alt: "New FPL Season Highlight", caption: "Team of the week 23 - Choco" }, 
       { src: "/imgs/fpl/fpl22.png", alt: "New FPL Season Highlight", caption: "Team of the week 22 - Panda" }, 
       { src: "/imgs/fpl/fpl21.png", alt: "New FPL Season Highlight", caption: "Team of the week 21 - Panda" }, 
       { src: "/imgs/fpl/fpl20.png", alt: "New FPL Season Highlight", caption: "Team of the week 20 - Choco" }, 

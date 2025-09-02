2a2edd736fad6eab08db6814ccd3af8f1933c693
Yury <yuryloguinov@192.168.1.13>
2025-09-02 22:04:09 +0200

    tour updates

--- app/fpl/page.tsx
+++ app/fpl/page.tsx
@@ -152,7 +152,8 @@ export default async function FPLPage() {
 
     // Current season highlights (update as new highlights happen)
     const currentSeasonHighlights = [
-      { src: "/imgs/fpl/fpl2526-2.png", alt: "New FPL Season Highlight", caption: "Team of the week 2 - Panda" },
+      { src: "/imgs/fpl/fpl2526-3.png", alt: "New FPL Season Highlight", caption: "Team of the week 3 - Vanilla" },
+      { src: "/imgs/fpl/fpl2526-2.png", alt: "New FPL Season Highlight", caption: "Team of the week 2 - Vanilla" },
       { src: "/imgs/fpl/fpl2526-1.png", alt: "New FPL Season Highlight", caption: "Team of the week 1 - Panda" },
       // Add more current season images as they happen
     ]
